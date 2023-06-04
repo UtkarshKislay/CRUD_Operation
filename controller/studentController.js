1743dc92b3932092e06b0fc6f667b1320560e47d
UtkarshKislay <utkarsh2237@gamil.com>
2023-06-04 13:35:03 +0530

    setting routes

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,8 +1,29 @@
-
+import StudentMode from '../models/StudentMode.js';
 class StudentController{
-    static getAllDocs=(req,res)=>{
-        res.render('index');
+    static createDoc=(req,res)=>{
+        res.redirect('/student');
     }
+    static getAllDocs=async(req,res)=>{
+        try{
+            const result=await StudentMode.find();
+            console.log(result);
+            res.render("index",{data:result});
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    static editDoc=(req,res)=>{
+        res.render('edit');
+    }
+    static updateDocById=(req,res)=>{
+        res.redirect('/student');
+    }
+
+    static deleteById=(req,res)=>{
+        res.redirect('/student');
+    }
+//crud
 }
 
 export default StudentController;
