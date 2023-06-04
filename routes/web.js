55ad18a46956bd3fb005edb3db30d5759fced55e
UtkarshKislay <utkarsh2237@gamil.com>
2023-06-04 12:21:23 +0530

    change file name from indes to index

--- routes/web.js
+++ routes/web.js
@@ -3,4 +3,8 @@ import StudentController from '../controller/studentController.js';
 const router =express.Router();
 
 router.get('/',StudentController.getAllDocs);
+router.get('/edit',(req,res)=>{
+    res.render('edit');
+});
+
 export default router;
